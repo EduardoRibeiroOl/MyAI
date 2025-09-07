6946f4cccb5da25b81c206f61d32461ca7961e5f
Eduardo Ribeiro Silva <eduardoolive777@gmail.com>
2025-09-07 10:56:14 -0300

    normalizate

--- main.py
+++ main.py
@@ -3,26 +3,43 @@
 from PIL import Image
 import numpy as np
 
+
+def normalization(red, green, blue):
+    gray_escale = (red * 0.299 + green * 0.587 + blue * 0.114).astype(np.uint8)
+    neuron_value = gray_escale / 255
+    return neuron_value
+
 def input_data_network(inputs, weights, bias):
     img = Image.open(r"C:\Users\pottv\Downloads\eletron\ai\image.jpg")
     matriz = np.array(img)
+    CHANNELS = 3 # 3 canais de cores (RGB), se quiser mudar para hexa tem que lembrar de mudar aqui
+    
+    height, width, canais = matriz.shape
+    neuron_matriz = np.zeros((height, width)) # matriz de neuronios que vai receber os valores normalizados
 
-    print("Tipo:", type(matriz))  # tipo de array que a matriz será
-    print("Formato (h, w, c):", matriz.shape) # chape da matriz (height, width, channels) 
-    print("Pixel [0,0]:", matriz[0, 0]) # Matriz tridimensional, chamado de tensor por ser entrada do nosso sistema. Para uma imagem colorida, teremos 3 canais de cores (RGB) 
+    #print("Matriz:", matriz)  # matriz 3d
+    #print("Tipo:", type(matriz))  # tipo de array que a matriz será
+    #print("Formato (h, w, c):", matriz.shape) # chape da matriz (height, width, channels) 
+    #print("Pixel [0,0]:", matriz[0, 0]) # Matriz tridimensional, chamado de tensor por ser entrada do nosso sistema. Para uma imagem colorida, teremos 3 canais de cores (RGB) 
     
     # Processo de normalização; Acbamos de alocar um tensor de 3 dimensôes, mas temos que simplificar, podemos trabalhar com os valores separados ou 
     # transformar esses valores em escalas de cinza, ou seja, preto e branco.
     # formula de luminancia: 0.299*R + 0.587*G + 0.114*B
     
-    
-    matriz_normalizada = (matriz[0,0,0] * 0.299 + matriz[0,0,1] * 0.587 + matriz[0,0,2] * 0.114).astype(np.uint8)
-    cinza_normalizado = matriz_normalizada / 255
+
+    for h in range(height):
+        for w in range(width):
+            neuron_value = normalization(matriz[h, w, 0], matriz[h, w, 1], matriz[h, w, 2])
+            neuron_matriz[h, w] = neuron_value
+    print(neuron_matriz[0,0]) # valor do neuronio na posição 0,0
+
+
     #A função astype(np.uint8) é utilizada para converter valores de uma array de float para um tipo de dados unsigned 8 bits. 
     # Isso significa que os valores vão de 0 a 255
     # Tem que fazer a entrada ser enfileirada e ser transformada em uma matriz de 1 dimensão
-    input = cinza_normalizado.flatten()
-    print("Matriz normalizada:", matriz_normalizada) 
+    #input = normal_gray.flatten() # flatten transforma a matriz em uma lista de valores
+    #print(input[0]) 
+
     
 input_data_network(0, 0, 0)
 # soma de todos os vallores . peso + bias é igual ao processo de ativação que o proximo neuronio vai receber
